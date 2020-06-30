9d29baf0d39c4349d0abee308002410f8440392d
jitndrasingh <j.s.r.p.vingarla@gmail.com>
2020-06-30 12:54:39 +0530

    Testing

---
+++ mfs1/src/mfs1/HelloWold.java
@@ -0,0 +1,14 @@
+package mfs1;
+
+public class HelloWold {
+
+	public static void main(String[] args) {
+        System.out.println("Hello!!");
+		
+		int i1 = 10;
+		int i2 = 20;
+		System.out.println(i1*i2);
+
+	}
+
+}
